2db091917358d8a79d21233afda9904d00354580
Heva <he.wu@linecorp.com>
2019-12-04 12:23:20 +0900

    Remove SceneDelegate & Add logic for Splash->Login page

--- RootViewControllerNavigation/RootViewControllerNavigation/RootViewControllers/RootViewController.swift
+++ RootViewControllerNavigation/RootViewControllerNavigation/RootViewControllers/RootViewController.swift
@@ -5,17 +5,51 @@
 //  Created by he.wu on 2019/12/03
 //  Copyright © 2019 He Wu. All rights reserved.
 //
-        
+
 
 import UIKit
 
-final class RootViewController {
-    let currentViewController: UIViewController
-    init(_ viewController: UIViewController) {
-        self.currentViewController = viewController
+final class RootViewController: UIViewController {
+    
+    private var root: UIViewController
+    
+    init() {
+        root = SplashViewController()
+        super.init(nibName: nil, bundle: nil)
     }
     
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .clear
+        
+        addChild(root)
+        root.view.frame = view.bounds
+        view.addSubview(root.view)
+        root.didMove(toParent: self)
+    }
+    
+    func switchToLogin() {
+        let loginVC = LoginViewController()
+        
+        addChild(loginVC)
+        loginVC.view.frame = view.bounds
+        view.addSubview(loginVC.view)
+        loginVC.didMove(toParent: self)
+        
+        // remove original one
+        root.willMove(toParent: nil)
+        root.view.removeFromSuperview()
+        root.removeFromParent()
+        
+        // replace root with loginVC
+        root = loginVC
+    }
 }
 
 
--- RootViewControllerNavigation/RootViewControllerNavigation/RootViewControllers/SplashViewController.swift
+++ RootViewControllerNavigation/RootViewControllerNavigation/RootViewControllers/SplashViewController.swift
@@ -5,36 +5,39 @@
 //  Created by he.wu on 2019/12/03
 //  Copyright © 2019 He Wu. All rights reserved.
 //
-        
+
 
 import UIKit
 
 class SplashViewController: UIViewController {
-    private let activityIndicator = UIActivityIndicatorView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.addSubview(activityIndicator)
-        activityIndicator.frame = view.bounds
-        activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.5)
-        
-        preloadServerAPI()
-    }
-    
-    private func preloadServerAPI() {
-        // mock server api connect
-        activityIndicator.startAnimating()
-        DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + 3) { [weak self] in
-            print("[API] Server API Preloaded")
-            self?.activityIndicator.stopAnimating()
-            
-            // temporary use UserDefaults for checking status
-            if UserDefaults.standard.bool(forKey: "He-Wu.RootViewControllerNavigation.login") {
-                // navigate to main page
-            } else {
-                // navigate to login page
-            }
-        }
-    }
+     private let activityIndicator = UIActivityIndicatorView()
+     
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          view.backgroundColor = .white
+          
+          view.addSubview(activityIndicator)
+          activityIndicator.frame = view.bounds
+          activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.5)
+          
+          preloadServerAPI()
+     }
+     
+     private func preloadServerAPI() {
+          // mock server api connect
+          activityIndicator.startAnimating()
+          DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) { [weak self] in
+               print("[API] Server API Preloaded")
+               self?.activityIndicator.stopAnimating()
+               
+               // temporary use UserDefaults for checking status
+               if UserDefaults.standard.bool(forKey: "He-Wu.RootViewControllerNavigation.login") {
+                    // navigate to main page
+               } else {
+                    // navigate to login page
+                    AppDelegate.shared.rootViewController.switchToLogin()
+               }
+          }
+     }
 }
